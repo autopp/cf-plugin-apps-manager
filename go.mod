498af0fb1da4337c4b39ab603a84f27e6b1440f1
autopp <autopp.inc@gmail.com>
2021-07-21 01:43:34 +0900

    test: use gomock

--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	code.cloudfoundry.org/cli v6.53.0+incompatible
-	github.com/onsi/ginkgo v1.16.4 // indirect
-	github.com/onsi/gomega v1.14.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.14.0
+	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 )
--- pkg/plugin/apps_manager_test.go
+++ pkg/plugin/apps_manager_test.go
@@ -1,20 +1,30 @@
-package plugin_test
+package plugin
 
 import (
+	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
-	cf "code.cloudfoundry.org/cli/plugin"
-
-	"github.com/autopp/cf-plugin-apps-manager/pkg/plugin"
+	"code.cloudfoundry.org/cli/plugin"
+	"github.com/autopp/cf-plugin-apps-manager/pkg/plugin/mock"
 )
 
 var _ = Describe("AppsManager", func() {
+	var ctrl *gomock.Controller
+	JustBeforeEach(func() {
+		ctrl = gomock.NewController(GinkgoT())
+	})
+
+	JustAfterEach(func() {
+		ctrl.Finish()
+	})
+
 	DescribeTable("GetMetadata",
 		func(v string, major, minor, build int) {
-			p := plugin.NewAppsManagerPlugin(nil, v)
-			Expect(p.GetMetadata().Version).To(Equal(cf.VersionType{Major: major, Minor: minor, Build: build}))
+			b := mock.NewMockBrowser(ctrl)
+			p := NewAppsManagerPlugin(b, v)
+			Expect(p.GetMetadata().Version).To(Equal(plugin.VersionType{Major: major, Minor: minor, Build: build}))
 		},
 		Entry(`"v1.2.3" -> (1, 2, 3)`, "v1.2.3", 1, 2, 3),
 		Entry(`"" -> (0, 0, 0)`, "", 0, 0, 0),
